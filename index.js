1c46291c7f7cc0922b048fc248700efeea7b9307
onurd <106928484+onurdzd@users.noreply.github.com>
2023-03-23 14:58:41 +0300

    env güncelleme

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(process.env.MESSAGE + " " + port);
 });
+
+server.get("/", (req,res) => {
+  res.send(process.env.MESSAGE + " ");
+});
